55d3a3c283c8c3abb0f413f2d2faab444585bc44
ReiiYuki <voraton.l@ku.th>
2017-07-27 16:38:53 +0900

    streamer

--- streamer/gaze_data_streamer.py
+++ streamer/gaze_data_streamer.py
@@ -1,2 +1,16 @@
-import zmq_receiver
+import zmq_receiver,zmq,json
 from sender import UDPSender
+
+streamer = UDPSender('127.0.0.1',5500)
+
+ctx = zmq.Context()
+
+req = zmq_receiver.init_requester(ctx,50124)
+
+g_sub = zmq_receiver.create_subscriber(ctx,req,u'gaze')
+
+while True :
+
+    g_data = zmq_receiver.get_data(g_sub)
+    norm_pos = g_data['norm_pos']
+    streamer.send('%s,%s'%(str(norm_pos[0]),str(norm_pos[1])))
--- streamer/pupil_data_streamer.py
+++ streamer/pupil_data_streamer.py
@@ -1,6 +1,8 @@
 import zmq_receiver,zmq,json
 from sender import UDPSender
 
+streamer = UDPSender('127.0.0.1',5500)
+
 ctx = zmq.Context()
 
 req = zmq_receiver.init_requester(ctx,50124)
@@ -12,4 +14,4 @@ while True :
 
     p0_data = zmq_receiver.get_data(p0_sub)
     p1_data = zmq_receiver.get_data(p1_sub)
-    print json.dumps([{'id':p0_data['id'],'norm_pos':p0_data['norm_pos'],'confidence':p0_data['confidence'],'timestamp':p0_data['timestamp']},{'id':p1_data['id'],'norm_pos':p1_data['norm_pos'],'confidence':p1_data['confidence'],'timestamp':p1_data['timestamp']}])
+    streamer.send(json.dumps([{'id':p0_data['id'],'norm_pos':p0_data['norm_pos'],'confidence':p0_data['confidence'],'timestamp':p0_data['timestamp']},{'id':p1_data['id'],'norm_pos':p1_data['norm_pos'],'confidence':p1_data['confidence'],'timestamp':p1_data['timestamp']}]))
